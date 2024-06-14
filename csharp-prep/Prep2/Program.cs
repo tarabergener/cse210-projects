86f0b6a55cf54a9299184996bb5f3cecc64cdcee
tarabergener <tara.kelly211@gmail.com>
2024-06-14 10:26:11 -0400

    grade changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your score out of 100 (ex. 94)? ");
+        int grade = Console.ReadLine();
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You got a B.");
+        }
+
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You got a C.");
+        }
+
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You got a D.");
+        }
+
+        else
+        {
+            Console.WriteLine("You got an F.");
+        }
     }
 }
