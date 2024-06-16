ad2bf46fc15d583b22811ac57afc33354102793e
tarabergener <tara.kelly211@gmail.com>
2024-06-16 00:56:27 -0400

    number updates list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = 1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+
+            number = int.Parse(userInput);
+
+            numbers.Add(number);
+        }
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"Sum: {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"Average: {average}");
+
+        int largestNumber = numbers.Max();
+        Console.WriteLine($"Largest Number: {largestNumber}");
     }
 }
