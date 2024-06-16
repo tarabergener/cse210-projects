12c40c4d433eb97ec9b0e9e796029831e2209da9
tarabergener <tara.kelly211@gmail.com>
2024-06-16 00:16:09 -0400

    secret number code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int secretNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        Console.WriteLine("Guess the secret number between 1 and 100!");
+
+        while (guess != secretNumber)
+        {
+            Console.Write("What is your guess? ");
+
+            string userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+
+            if (guess > secretNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < secretNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
