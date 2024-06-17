828e5633657200349395d7001e73ff517d16f062
tarabergener <tara.kelly211@gmail.com>
2024-06-17 19:05:14 -0400

    functions changes

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(squaredNumber, userName);
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favNumber = int.Parse(Console.ReadLine());
+        
+        return favNumber;
+    }
+    static int SquareNumber(int favNumber)
+    {
+        int squaredNumber = favNumber * favNumber;
+        return squaredNumber;
+    }
+    static void DisplayResult(int squaredNumber, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}.");
+    }
+
 }
+    
