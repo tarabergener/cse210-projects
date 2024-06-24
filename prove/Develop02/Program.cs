ec1b798d698266c8c85676d2e413f96bcb278a06
tarabergener <tara.kelly211@gmail.com>
2024-06-24 19:42:37 -0400

    commit site changes

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        displayMessage();
+
+        displayOptions();
+
+        
+    }
+
+    static void displayMessage()
+    {
+        Console.WriteLine("Welcome to the Journal Program!");
+    }
+
+    static void displayOptions()
+    {
+        List<string> options = new List<string> {"Write", "Display", "Load", "Save", "Quit"};
+
+        int selection = 1;
+
+        while (selection != 5) {
+
+            for (int i = 1; i = options; i++) {
+
+                Console.WriteLine(i);
+            }
+
+            Console.Write("Please select an option: ");
+            string userInput = Console.ReadLine();
+
+            selection = int.Parse(userInput);
+        }
     }
 }
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,20 @@
+class PromptGenerator
+{
+    public List<string> _prompts = new List<string>()
+    {
+        "What is the best thing you did today?",
+        "What did you do to help someone today?",
+        "What is a new skill you would like to develop?",
+        "Who is someone who has made you a better person?",
+        ""
+    };
+
+    public string GetRandomPrompt() 
+    {
+        Random randomGenerator = new Random();
+        int i = randomGenerator.Next(5);
+        string randomPrompt = _prompts[i];
+
+        return randomPrompt;
+    }
+}
