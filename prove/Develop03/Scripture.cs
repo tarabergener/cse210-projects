b18919d9d691407389a6ee8727e93859a49f0d21
tarabergener <tara.kelly211@gmail.com>
2024-06-26 22:48:01 -0400

    system bones

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,8 +1,29 @@
 using System;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
 
 public class Scripture
 {
     Reference _reference = new Reference("John", 3, 19);
     List<Word> _words;
 
+    public Scripture(Reference _reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public static string GetDisplayText()
+    {
+        return ---;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return ---; 
+    }
 }
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Concurrent;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public bool IsHidden()
+    {
+        return ---;
+    }
+
+    public string GetDisplayText()
+    {
+        return ---;
+    }
+
+}
